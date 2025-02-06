f632ed186df354590b521cc164baa08abc081bb3
Alessia Guadagnin Pattaro <alessiagp@proton.me>
2025-02-06 16:21:53 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
    name='alex',
    version='1.0',
    description='Postprocessing and analysis of data obtained from the EXCOGITO software',
+   long_description=open("README.md").read() if "README.md" else "",
    author='Alessia Guadagnin Pattaro',
    author_email='alessia.guadagnin@unitn.it',
-   packages=['alex'],  #same as name
+   packages=find_packages(),  #same as name
    entry_points = {
     'console_scripts': ['alex=alex.run:main']
     },
 
    install_requires=['numpy', 'pandas', 'scipy', 'mdtraj'], #external packages as dependencies
+   python_requires=">=3.12",
 )
